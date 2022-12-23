a844ba44df8ecb29c34c4856dd51c58fbf40f5c9
Roy Crivolotti <roycrivolotti@gmail.com>
2022-12-23 12:52:45 +0100

    Adding user model and tests + First migrations

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,25 @@
-from django.db import models  # noqa TODO: delete comment
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=355, unique=True)
+    name = models.CharField(max_length=355)
+    is_active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
