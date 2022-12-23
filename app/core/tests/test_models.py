05db3513c4031f3596ac2952d071595168411499
Roy Crivolotti <roycrivolotti@gmail.com>
2022-12-23 13:24:50 +0100

    Minor edit

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,8 +4,8 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
     def test_create_user_with_email_ok(self):
-        email = "asd@gmail.com"
-        password = "123"
+        email = 'asd@gmail.com'
+        password = '123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
@@ -16,13 +16,17 @@ class ModelTests(TestCase):
 
     def test_new_user_with_email_normalized(self):
         emails = [
-            ["asd1@GMAIL.com", "asd1@gmail.com"],
-            ["ASD2@gmail.com", "ASD2@gmail.com"],
-            ["ASD3@GMAIL.com", "ASD3@gmail.com"],
-            ["ASD4@gmail.COM", "ASD4@gmail.com"],
-            ["ASD5@GMAIL.COM", "ASD5@gmail.com"],
+            ['asd1@GMAIL.com', 'asd1@gmail.com'],
+            ['ASD2@gmail.com', 'ASD2@gmail.com'],
+            ['ASD3@GMAIL.com', 'ASD3@gmail.com'],
+            ['ASD4@gmail.COM', 'ASD4@gmail.com'],
+            ['ASD5@GMAIL.COM', 'ASD5@gmail.com'],
         ]
 
         for email, expected in emails:
-            user = get_user_model().objects.create_user(email, "password123")
+            user = get_user_model().objects.create_user(email, 'password123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('email', 'password123')
